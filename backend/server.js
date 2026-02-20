2debd5b7fcc79f1574d3be0c83f33b96f8bc57a5
Apoorv Verma <apvstmp@gmail.com>
2026-02-20 22:49:45 +0530

    Modifications on 20/2...made home, planner and api call

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = 5000;
-
+//CRUD : Create Read Update(patch) Delete
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -11,4 +11,8 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.get("/addUser",(req,res) => {
+  res.send("TravelMania BAckend is here");
 });
