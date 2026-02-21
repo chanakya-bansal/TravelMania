d68daa16ab75dceb3d01486eb2df20ec70e4ba51
Soumalya35 <soumalyakuila@gmail.com>
2026-02-21 21:26:28 +0530

    pic

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import "../styles/Home.css";
 import img1 from "../assets/images/beach.jpg";
 import img2 from "../assets/images/citylife.jpg";
 import img3 from "../assets/images/monuments.jpg";
-import img4 from "../assets/images/mountains.jpg";
+import img4 from "../assets/images/mountain2.jpg";
 import img5 from "../assets/images/wildlife.jpg";
 const slides=[img1,img2,img3,img4,img5];
 
