5c3a78e9c57f844c06857671e299cffc349baa12
unknown <sweethomemrt@gmail.com>
2026-02-21 16:18:33 +0530

    added header and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,64 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+
 import Home from "./pages/Home";
 import Planner from './pages/Planner';
 
-// function App() {
-//   const [count, setCount] = useState(0)
-//   // const [count2, setCount2] = useState(0)
-
-
-//   // function Click()
-//   // {
-//   //   console.log("Hello World");
-//   //   // setCount((count)=>count+1);  //Infinite Loop bann rha hai
-//   // }
-
-//   // useEffect(() => {
-//   //   Click();
-//   // }, [count]);
-
-
-  
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//           {/* /////////'/vite.svg'////////// */}
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <button onClick={Click()}>Click</button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
 
 function App()
 {
   return(
     <BrowserRouter>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/planner" element={<Planner />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
