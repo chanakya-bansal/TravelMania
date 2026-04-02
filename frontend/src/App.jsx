37d4600b658058aa4b966430e56ef3ebe7b33ab3
Khushi Srivastava <happen.ness04@gmail.com>
2026-04-02 23:29:33 +0530

    added the explore page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Planner from './pages/Planner';
 import About from './pages/About';
-
+import Explore from "./pages/Explore";
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
 
@@ -24,6 +24,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/planner" element={<Planner />} />
         <Route path="/about" element={<About />} />
+        <Route path="/explore" element={<Explore />} />
       </Routes>
       <Footer />
     </BrowserRouter>
